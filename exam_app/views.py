fc631e4b24ca0686390453542ceb7a63e44edf02
kalaiselvanj <110382458+kalaiselvanj@users.noreply.github.com>
2023-06-20 12:04:16 +0530

    previous changes

--- exam_app/views.py
+++ exam_app/views.py
@@ -969,6 +969,7 @@ def submit_answers(request):
 
     
 
+# Load the Haar Cascade classifier
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 import time
@@ -1035,13 +1036,14 @@ def gen():
         # Yield the frame to the calling function
         yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-        
+
 @gzip.gzip_page
-def dynamic_stream(request):
+def dynamic_stream(request,stream_path="video"):
     try :
         return StreamingHttpResponse(gen(),content_type="multipart/x-mixed-replace;boundary=frame")
     except :
         return "error"
+
     
 # define function to generate video frames
 def video_feed():
@@ -1061,6 +1063,91 @@ def video_feed():
 @gzip.gzip_page
 def video(request):
     return StreamingHttpResponse(video_feed(), content_type='multipart/x-mixed-replace; boundary=frame')
+
+# import cv2
+# import datetime
+# import os
+# import time
+# from django.http import HttpResponse
+# from django.views.decorators.csrf import csrf_exempt
+# from channels.layers import get_channel_layer
+# from asgiref.sync import async_to_sync
+
+# def gen():
+#     face_cascade = cv2.CascadeClassifier('path/to/your/haarcascade_frontalface_default.xml')
+
+#     # Open the camera
+#     cap = cv2.VideoCapture(0)
+
+#     # Initialize image counter
+#     img_counter = 0
+
+#     # Initialize the flag for capturing an image
+#     capture_flag = False
+
+#     while True:
+#         # Read a frame from the camera
+#         ret, frame = cap.read()
+#         frame = cv2.flip(frame, 1)
+
+#         # Convert to grayscale
+#         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+#         # Detect faces in the frame
+#         faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+
+#         # Draw a rectangle around each detected face
+#         for (x, y, w, h) in faces:
+#             cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+#         # Display the number of faces detected
+#         cv2.putText(frame, ' ' + str(len(faces)), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+#         if len(faces) > 1:
+#             now = datetime.datetime.now()
+#             p = os.path.sep.join(['img', "More than one face detected{}.png".format(str(now).replace(":", ''))])
+
+#             # Set the capture flag to True and save the image to the disk
+#             capture_flag = True
+#             cv2.imwrite(p, frame)
+#             img_counter += 1
+#             # Add a delay of 2 seconds if the capture flag is set to True
+#             if capture_flag:
+#                 time.sleep(2)
+#                 capture_flag = False
+
+#         if len(faces) == 0:
+#             now = datetime.datetime.now()
+#             p = os.path.sep.join(['img', "No face detected{}.png".format(str(now).replace(":", ''))])
+
+#             # Set the capture flag to True and save the image to the disk
+#             capture_flag = True
+#             cv2.imwrite(p, frame)
+#             img_counter += 1
+#             # Add a delay of 2 seconds if the capture flag is set to True
+#             if capture_flag:
+#                 time.sleep(2)
+#                 capture_flag = False
+
+#         # Resize and encode the frame
+#         frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+#         ret, buffer = cv2.imencode('.jpg', frame)
+#         frame = buffer.tobytes()
+
+#         # Yield the frame to the calling function
+#         yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+# @csrf_exempt
+# def video_stream(request):
+#     if request.is_websocket():
+#         channel_layer = get_channel_layer()
+#         async_to_sync(channel_layer.group_add)('video_stream', request.websocket)
+
+#         for frame in gen():
+#             async_to_sync(request.websocket.send)(frame)
+
+#     return HttpResponse()
+
     
 
 def exam_portal(request):
@@ -1110,3 +1197,6 @@ def exam_portal(request):
             print(e)
             return HttpResponseBadRequest("Bad Request")
     return redirect('login')
+
+def camera_part(request):
+    return render(request,"exam_portal/camera_part.html")
